1e1699972786dce4e013460df600d26d73154a69
Marta <martamunoz85@gmail.com>
2022-11-30 18:36:22 +0100

    Added new background hero

---
+++ js/script.js
@@ -0,0 +1,8 @@
+const navbar = document.querySelector(".navbar-custom");
+window.onscroll = () => {
+  if (window.scrollY > 50) {
+    navbar.classList.add("scrolled");
+  } else {
+    navbar.classList.remove("scrolled");
+  }
+};
